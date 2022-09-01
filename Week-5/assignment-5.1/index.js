78e0cb1ba35cf2c32dcb0a0721b4c95199234128
khande1n <nix8419@gmail.com>
2022-09-01 09:43:21 +0530

    generator returning promise

--- Week-5/assignment-5.1/index.js
+++ Week-5/assignment-5.1/index.js
@@ -1,35 +1,39 @@
 // Generator function
+const promise1 = new Promise((resolve, reject) => {
+    resolve(
+        setTimeout(() => {
+            console.log("timeout run");
+        }, 2000)
+    );
+});
+
+const promise2 = new Promise((resolve, reject) => {
+    resolve("task 2 is done");
+});
+
+const promise3 = new Promise((resolve, reject) => {
+    resolve("task 3 is done");
+});
+
 function* getData(){
-    let i = 1;
-    while(true){
-        yield i;
-        i++;
-    }
+    yield promise1;
+    yield promise2;
+    yield promise3;
 }
 
 const generatorFunc = getData();
 
 // async/await function
-
-function doTask1(generatorFunc){
-    return generatorFunc.next().value*2;
-}; 
-
-function doTask2(data1){
-    return data1.toString();
-}
-
-function doTask3(data2){
-    return parseFloat(data2).toFixed(3);
-}
-
 const result = async function taskDoer() {
-    const data1 = await doTask1(generatorFunc);
-    const data2 = await doTask2(data1);
-    const data3 = await doTask3(data2);
-    // console.log(data1, typeof data1);
-    // console.log(data2, typeof data2);
-    // console.log(data3, typeof data3);
+    await generatorFunc.next().value.then((data) => {
+        // console.log(data);
+    });
+    await generatorFunc.next().value.then((data) => {
+        console.log(data);
+    });
+    await generatorFunc.next().value.then((data) => {
+        console.log(data);
+    });
 }
 
 result();
