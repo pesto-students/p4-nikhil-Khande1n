188529c94a469e4b1290413376302e424d3d2055
khande1n <nix8419@gmail.com>
2022-10-04 17:42:26 +0530

    phase 1 done: on click activated

---
+++ Week-9/todolist/src/App.js
@@ -0,0 +1,48 @@
+// import logo from './logo.svg';
+import './App.css';
+import { EACHDAY } from './eachday';
+import React from 'react';
+
+// function App() {
+//   return (
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     // </div>
+//     <div>
+//       <EACHDAY />
+//     </div>
+//   );
+// }
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+      eachDayList: []
+    }
+  }
+
+
+  render() {
+    return(
+      <EACHDAY date={this.state.date}/>
+    )
+  }
+}
+
+export default App;
---
+++ Week-9/todolist/src/eachtask.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class EACHTASK extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <input></input>
+        )
+    }
+}
