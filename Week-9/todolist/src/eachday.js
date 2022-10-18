9b8965a75df9b0ec1f0fc4cb123545821d0b3025
khande1n <nix8419@gmail.com>
2022-10-18 15:16:30 +0530

    todo list working

--- Week-9/todolist/src/eachday.js
+++ Week-9/todolist/src/eachday.js
@@ -5,44 +5,47 @@ import { EACHTASK } from "./eachtask";
 export class EACHDAY extends React.Component {
     constructor(props) {
         super(props);
-        // console.log(props);
 
         this.state = {
-            counter: 1,
-            taskList: []
+            counter: 0,
+            tasks: []
         }
 
         this.addTask = this.addTask.bind(this);
     }
 
+    deleteTask = async(id) => {
+        console.log(id);
+        let newList = this.state.tasks.filter((task, index) => index + 1 !== id);
+        console.log("newlist", newList);
+        await this.setState((state, props) => ({
+            tasks: [...newList]
+        }));
+        
+    }
+
     addTask = async() => {
-        console.log("before counter", this.state.counter);
         await this.setState((state, props) => ({
             counter: state.counter + 1
         }));
-        console.log("after counter", this.state.counter);
+
         await this.setState((state, props) => ({
-            taskList: [...state.taskList, state.counter ]
+            tasks: [...state.tasks, <EACHTASK key={state.counter} id={state.counter} deleteTask={this.deleteTask}/>]
         }));
     }
 
     render() {
-        let tasks = null;
-        if(this.state.taskList) {
-            console.log(this.state.taskList, "tasklist1");
-            let tasks = this.state.taskList.map((data) => {
-                return (<EACHTASK key={this.state.counter}/>)
-            });
-            console.log(tasks, "tasklist2");
-        }
-
         return (
             <>
             <h1>Task List for time: {this.props.date.toLocaleTimeString()}</h1>
-            <ul>
-                {tasks ? tasks: null}
-            </ul>
             <button onClick={this.addTask}>Click to add task</button>
+            <ul>
+                {
+                    this.state.tasks.map((key, value) => {
+                        return key;
+                    })
+                }
+            </ul>
             </>
             
         );
--- Week-9/todolist/src/eachtask.js
+++ Week-9/todolist/src/eachtask.js
@@ -3,11 +3,42 @@ import React from 'react';
 export class EACHTASK extends React.Component {
     constructor(props) {
         super(props);
+        console.log(props);
+
+        this.state = {
+            isChecked: false,
+            deleteComponent: false
+        }
+
+        // this.complete = this.complete.bind(this);
     }
+
+    toggleCheck(id) {
+        this.setState((state,props) => {
+            return {isChecked: !state.isChecked}
+        }, () => {
+            console.log(this.state.isChecked);
+            if(this.state.isChecked) {
+                const el = document.getElementById(id);
+                el.getElementsByTagName('input')[1].setAttribute('style', 'background-color: red;');
+            } else {
+                const el = document.getElementById(id);
+                el.getElementsByTagName('input')[1].setAttribute('style', 'background-color: white;');
+            }
+        }); 
+    }
+
 
     render() {
         return (
-            <input></input>
+            <>
+            <div style={{display: 'block', margin: '10px'}} id={this.props.id}>
+                <input type={'checkbox'} onClick={this.toggleCheck.bind(this, this.props.id)}></input>
+                <input placeholder='enter text here' ></input>
+                <button onClick={() => this.props.deleteTask(this.props.id)}>Delete</button>
+            </div>
+               
+            </>
         )
     }
 }
